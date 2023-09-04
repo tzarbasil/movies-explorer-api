8b58f7f47598c39c53928498381324cc1b60420c
tzarbasil <atreides03@mail.ru>
2023-09-04 23:31:04 +0200

    review fix

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,10 @@ const router = require('express').Router();
 const users = require('./users');
 const movies = require('./movies');
 
-// Импорт миддлвэра для авторизации
 const { auth } = require('../middlewares/auth');
 
 const NotFoundError = require('../errors/NotFoundError');
 
-// Импорт контроллеров и валидаторов
 const { createUser, login } = require('../controllers/users');
 const { validateCreateUser, validateLoginUser } = require('../middlewares/celebrate');
 
@@ -17,7 +15,6 @@ router.post('/signin', validateLoginUser, login);
 router.use('/users', auth, users);
 router.use('/movies', auth, movies);
 
-// роут для запросов по несуществующим URL
 router.use('*', auth, (req, res, next) => {
   next(new NotFoundError('Ресурс не найден. Проверьте URL и метод запроса'));
 });
